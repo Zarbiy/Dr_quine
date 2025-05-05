93c436e597a4a52dba69475f705b630d52571395
Corentin Nguyen <cornguye@bess-f2r2s5.clusters.42paris.fr>
2025-05-05 11:03:10 +0200

    finish Grace assembleur

--- Grace/Asm/Grace.s
+++ Grace/Asm/Grace.s
@@ -3,30 +3,38 @@
 %define FILENAME "Grace_kid.s"
 
 section .data
-    msg db "section .data%1$c    msg db %2$c%4$s%2$c,0%1$c%1$csection .text%1$c    global _start%1$c    extern printf, open, write, close%1$c%1$c; this is a comment%1$c%macro START 0%1$c_start:%1$c    mov rdi, %2$c%4$s%2$c%1$c    mov rsi, 577%1$c    mov rdx, 0777%1$c    xor rax, rax%1$c    mov rax, [rel open wrt ..got]%1$c    call rax%1$c    mov rdi, rax%1$c    lea rsi, [rel msg]%1$c    xor rax, rax%1$c    mov rax, [rel printf wrt ..got]%1$c    call rax%1$c    xor rax, rax%1$c    mov rax, [rel close wrt ..got]%1$c    call rax%1$c    ret%1$c%endmacro%1$c%1$c START",0
+    msg db "%%define LF 10%1$c%%define DQ 34%1$c%%define FILENAME %2$cGrace_kid.s%2$c%1$c%1$csection .data%1$c    msg db %2$c%3$s%2$c,0%1$c    filename db FILENAME, 0%1$c%1$csection .text%1$c    global _start%1$c    extern dprintf, open, write, close, exit%1$c%1$c; this is a comment%1$c%%macro START 0%1$c_start:%1$c    mov rdi, filename%1$c    mov rsi, 577%1$c    mov rdx, 0o777%1$c    xor rax, rax%1$c    mov rax, [rel open wrt ..got]%1$c    call rax%1$c    mov rdi, rax%1$c    lea rsi, [rel msg]%1$c    mov rdx, LF%1$c    mov rcx, DQ%1$c    lea r8, [rel msg]%1$c    xor rax, rax%1$c    mov rax, [rel dprintf wrt ..got]%1$c    call rax%1$c    mov rdi, 0%1$c    xor rax, rax%1$c    mov rax, [rel close wrt ..got]%1$c    call rax%1$c    mov rdi, 0%1$c    xor rax, rax%1$c    mov rax, [rel exit wrt ..got]%1$c    call rax%1$c%%endmacro%1$c%1$cSTART",0
+    filename db FILENAME, 0
 
 section .text
     global _start
-    extern printf, open, write, close
+    extern dprintf, open, write, close, exit
 
 ; this is a comment
 %macro START 0
 _start:
-    mov rdi, FILENAME
+    mov rdi, filename
     mov rsi, 577
-    mov rdx, 0777
+    mov rdx, 0o777
     xor rax, rax
     mov rax, [rel open wrt ..got]
     call rax
     mov rdi, rax
     lea rsi, [rel msg]
+    mov rdx, LF
+    mov rcx, DQ
+    lea r8, [rel msg]
     xor rax, rax
-    mov rax, [rel printf wrt ..got]
+    mov rax, [rel dprintf wrt ..got]
     call rax
+    mov rdi, 0
     xor rax, rax
     mov rax, [rel close wrt ..got]
     call rax
-    ret
+    mov rdi, 0
+    xor rax, rax
+    mov rax, [rel exit wrt ..got]
+    call rax
 %endmacro
 
 START
